ed28456a37e1bbf6a3f32364ad9d7c759f8dd144
brailsmt <brailsmt@a272ff57-b438-0410-9c92-d377374fc898>
2007-08-30 02:37:19 +0000

    "Improved" the helper script.
    git-svn-id: https://svn.code.sf.net/p/mbrt/code/trunk@13 a272ff57-b438-0410-9c92-d377374fc898

--- trace.sh
+++ trace.sh
@@ -3,12 +3,22 @@
 if [[ $# == 0 ]]; then
     echo Usage:
     echo
-    echo   trace.sh  scenefile
+    echo   trace.sh  path/to/scenefile
     echo
     exit 1
 fi
-./rt --scene scenes/$1.xml
-ppmtojpeg renders/$1.ppm > renders/$1.jpg
+#get the name of the rendered image from the XML file.
+imgname=`grep 'file name=' $1 | gawk -F'"' '{ print $2 }' | xargs -i{} basename {} .ppm`
+
+./rt --scene $1
+
+# Use imagemagick instead of ppmtojpeg if it exists
+which convert > /dev/null 2>&1
+if [[ $? == 0 ]]; then
+    convert renders/$imgname.ppm renders/$imgname.png
+else
+    ppmtojpeg renders/$imgname.ppm > renders/$imgname.jpg
+fi
 
 # mbrt is trashing my terminal. Could be a Cygwin issue
 #
