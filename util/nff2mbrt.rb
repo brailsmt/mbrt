01c75cdc4bd0adb04c09d69856b12c4d849e994f
brailsmt <brailsmt@a272ff57-b438-0410-9c92-d377374fc898>
2007-10-23 04:33:13 +0000

    Adding a script to convert from nff files to mbrt's XML scene file definition.
    git-svn-id: https://svn.code.sf.net/p/mbrt/code/trunk@131 a272ff57-b438-0410-9c92-d377374fc898

---
+++ util/nff2mbrt.rb
@@ -0,0 +1,190 @@
+#!/usr/bin/env ruby
+# file name:  nff2mbrt.rb
+# author:     Michael Brailsford
+# created:    Mon Oct 22 22:02:13 -0500 2007
+# copyright:  (c) 2007 Michael Brailsford
+# version:    $Revision$
+# contents:   This script reads the contents of a file in NFF format and
+#             translates it into mbrt's XML format.
+
+#{{{
+class Point
+    attr_accessor :x, :y, :z
+    def initialize(x, y, z)
+        @x = x
+        @y = y
+        @z = z
+    end
+    def to_s
+        "(#{@x}, #{@y}, #{@z})"
+    end
+end
+#}}}
+#{{{
+class Color
+    attr_accessor :name, :red, :green, :blue
+    def initialize(name, r, g, b)
+        @name  = name
+        @red   = r
+        @green = g
+        @blue  = b
+    end
+    def to_s
+        "<color name='#{@name}' red='#{@red}' green='#{@red}' blue='#{@red}' />"
+    end
+end
+#}}}
+#{{{
+class View
+    attr_accessor :camera
+    def initialize(camera_pt)
+        @camera = camera_pt
+    end
+end
+#}}}
+#{{{
+class Sphere
+    attr_accessor :origin, :r, :clr
+    def initialize(x, y, z, r, clr="def")
+        @origin = Point.new(x,y,z)
+        @r = r
+        @color = clr
+    end
+    def to_s
+        "<sphere center='#{@origin.to_s}' radius='#{@r}' color='#{@color}' />"
+    end
+end
+#}}}
+#{{{
+class Light < Sphere
+    attr_accessor :color
+    def initialize(x, y, z, clr="def_light")
+        super(x,y,z,0.001,clr)
+        @color = clr
+    end
+end
+#}}}
+#{{{
+class Material
+    def initialize(clr, diffuse, specular, phong, transmittance, refract_index)
+        @color         = clr
+        @diffuse       = diffuse
+        @specular      = specular
+        @phong         = phong
+        @transmittance = transmittance
+        @refract_index = refract_index
+    end
+    def to_s
+        "<material name='#{@color.name}' type='solid' color='#{@color.name}' />"
+    end
+end
+#}}}
+#{{{
+class ConeCylinder
+    def initialize(base_pt, base_r, apex_pt, apex_r)
+        @basept = base_pt
+        @baser = base_r
+        @apexpt = apex_pt
+        @apexr = apex_r
+    end
+    def to_s
+        "<conic base='#{@basept.to_s}' base_radius='#{@baser}' apex='#{@apexpt.to_s}' apex_radius='#{@apexr}' />"
+    end
+end
+#}}}
+#{{{
+class Polygon
+    attr_accessor :vertices
+    def initialize
+        @vertices = Array.new
+    end
+    def <<(pt)
+        @vertices << pt
+    end
+end
+#}}}
+#{{{
+class Mesh
+    attr_accessor :vertices_normals
+    def initialize
+        @vertices_normals = Array.new
+    end
+    def <<(pt, normal)
+        @vertices_normals << [pt, normal]
+    end
+end
+#}}}
+
+#{{{
+class Meta
+    attr_accessor :width, :height
+    def initialize
+    end
+    def to_s
+        str = <<-EOL
+        <meta>
+            <width     pixels="#{@width}"  /> 
+            <height    pixels="#{@height}" /> 
+            <subpixels square_of="5"       /> 
+            <recurse   max_depth="15"      /> 
+        </meta>
+        EOL
+    end
+end
+#}}}
+#{{{
+class Mbrt
+    attr_accessor :meta, :camera, :colors, :materials, :light, :objects
+    def initialize
+        @colors    = Array.new
+        @materials = Array.new
+        @light     = Array.new
+        @objects   = Array.new
+    end
+    def to_s
+        str = ""
+        @colors.each { |c| str << c.to_s + "\n" }
+        @materials.each { |m| str << m.to_s + "\n" }
+        @objects.each { |obj| str << obj.to_s + "\n" }
+        str
+    end
+end
+#}}}
+
+def main
+    mbrt = Mbrt.new
+    cname = "color"
+    cnum = 0
+    nff = File.open(ARGV[0])
+    nff.each { |line|
+        line = line.split('#')[0]
+        next if line =~ /^#/
+        case line
+        when /^s/
+            (type, x, y, z, r) = line.split
+            mbrt.objects << Sphere.new(x,y,z,r,cname+cnum.to_s)
+        when /^c/
+            (type, x1, y1, z1, r1, x2, y2, z2, r2) = line.split
+            p1 = Point.new(x1,y1,z1)
+            p2 = Point.new(x2,y2,z2)
+            mbrt.objects << ConeCylinder.new(p1, r1, p2, r2)
+        when /^f/
+            (type, r, g, b, diff, spec, ph, trans, refract) = line.split
+            cnum += 1
+            clr = Color.new(cname+cnum.to_s, r, g, b)
+            mbrt.colors << clr
+            mbrt.materials << Material.new(clr, diff, spec, ph, trans, refract)
+        end
+    }
+
+    puts mbrt.to_s
+end
+
+main()
+
+=begin
+	change log
+
+	$Log$
+=end
+
